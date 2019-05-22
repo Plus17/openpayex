1626e819878f4d9d49cfdbaebbd02244af79f582
Plus17 <sergio.valente@resuelve.mx>
2019-05-21 19:20:18 -0500

    initial commit

---
+++ lib/openpayex.ex
@@ -0,0 +1,18 @@
+defmodule Openpayex do
+  @moduledoc """
+  Documentation for Openpayex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Openpayex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/openpayex_test.exs
@@ -0,0 +1,8 @@
+defmodule OpenpayexTest do
+  use ExUnit.Case
+  doctest Openpayex
+
+  test "greets the world" do
+    assert Openpayex.hello() == :world
+  end
+end
