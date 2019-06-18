b0c85eefdd1423c51759a588d3a95ab8fa149487
Plus17 <sergio.valente@resuelve.mx>
2019-06-18 18:37:23 -0500

    explicit examples

--- lib/openpayex/customers.ex
+++ lib/openpayex/customers.ex
@@ -22,7 +22,19 @@ defmodule Openpayex.Customers do
    }
 
   iex> Openpayex.Customers.create(params)
-  {:ok, response}
+  {:ok,
+    %{
+      "address" => nil,
+      "clabe" => nil,
+      "creation_date" => "2019-06-18T18:34:32-05:00",
+      "email" => "customer_email@me.com",
+      "external_id" => nil,
+      "id" => "a8ujqsvn9e642lhpa6uj",
+      "last_name" => nil,
+      "name" => "customer name",
+      "phone_number" => nil
+    }
+  }
   ```
   """
   @spec create(map) :: map
@@ -54,7 +66,27 @@ defmodule Openpayex.Customers do
   customer_id = "a4wgoshubzmsjqujdsig"
 
   iex> Openpayex.Customers.update(params, customer_id)
-  {:ok, response}
+  {:ok,
+    %{
+      "address" => %{
+        "city" => "Queretaro",
+        "country_code" => "MX",
+        "line1" => "Calle 10",
+        "line2" => "col. san pablo",
+        "line3" => "entre la calle 1 y la 2",
+        "postal_code" => "76000",
+        "state" => "Queretaro"
+      },
+      "clabe" => nil,
+      "creation_date" => "2019-06-18T18:18:14-05:00",
+      "email" => "customer_email@me.com",
+      "external_id" => nil,
+      "id" => "a4wgoshubzmsjqujdsig",
+      "last_name" => nil,
+      "name" => "customer name",
+      "phone_number" => "44209087654"
+    }
+  }
   ```
   """
   @spec update(map, String.t) :: {:ok, map}
@@ -71,7 +103,27 @@ defmodule Openpayex.Customers do
   customer_id = "a4wgoshubzmsjqujdsig"
 
   iex> Openpayex.Customers.get(ustomer_id)
-  {:ok, response}
+  {:ok,
+    %{
+      "address" => %{
+        "city" => "Queretaro",
+        "country_code" => "MX",
+        "line1" => "Calle 10",
+        "line2" => "col. san pablo",
+        "line3" => "entre la calle 1 y la 2",
+        "postal_code" => "76000",
+        "state" => "Queretaro"
+      },
+      "clabe" => nil,
+      "creation_date" => "2019-06-18T18:18:14-05:00",
+      "email" => "customer_email@me.com",
+      "external_id" => nil,
+      "id" => "a4wgoshubzmsjqujdsig",
+      "last_name" => nil,
+      "name" => "customer name",
+      "phone_number" => "44209087654"
+    }
+  }
   ```
   """
   @spec get(String.t) :: {:ok, map}
