28e3fef5cdb4f2b0328f3f9aa30aeea40f7e0d97
Plus17 <sergio.valente@resuelve.mx>
2019-06-19 16:37:11 -0500

    process empty response

--- lib/openpayex/openpay/client.ex
+++ lib/openpayex/openpay/client.ex
@@ -13,23 +13,24 @@ defmodule Openpayex.OpenPay.Client do
     "#{base_url()}/#{endpoint}"
   end
 
-  def process_request_headers(_headers)do
+  def process_request_headers(_headers) do
     [
       {"Content-Type", "application/json"}
     ]
   end
 
   def process_request_options(_options) do
-   credentials = Application.get_env(:openpayex, :private_key)
-   [hackney: [basic_auth: {"#{credentials}:", ""}]]
+    credentials = Application.get_env(:openpayex, :private_key)
+    [hackney: [basic_auth: {"#{credentials}:", ""}]]
   end
 
-  def process_request_body(body)do
+  def process_request_body(body) do
     Jason.encode!(body)
   end
 
-  def process_response_body(body)do
+  def process_response_body(""), do: ""
+
+  def process_response_body(body) do
     Jason.decode!(body)
   end
-
 end
